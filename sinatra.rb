eaee91a4f6da03c43c858f8cafb545a4e2e3eff9
Peter Drake <peter.drake@acquia.com>
2018-12-26 23:15:06 +0000

    Try adding a working lambda handler.

--- sinatra.rb
+++ sinatra.rb
@@ -7,11 +7,20 @@ require_relative 'lib/westwood'
 
 Bugsnag.configure do |config|
   config.api_key = "eb7f40797e4e348e5c4c77615d82b026"
+  project_root = File.expand_path(File.dirname(__FILE__))
 end
 
 set :raise_errors, true
 
 use Bugsnag::Rack
+
+before do
+  if request.body.size > 0
+    request.body.rewind
+    @params = Sinatra::IndifferentHash.new
+    @params.merge!(JSON.parse(request.body.read))
+  end
+end
 
 post '/' do
   Ralyxa::Skill.handle(request)
